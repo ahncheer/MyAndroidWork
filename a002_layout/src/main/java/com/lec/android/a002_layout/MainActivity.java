b256b7c2fa3795263dc2ce507e244bcf223153e1
ahncheer <ahncheer@naver.com>
2020-04-14 10:44:08 +0900

    집에서 수정 2020-04-14 10:42:00

--- a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
+++ a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear3);
+        setContentView(R.layout.linear2);
+
+        int sss = 10;
+        String ttt = "";
     }
 }
