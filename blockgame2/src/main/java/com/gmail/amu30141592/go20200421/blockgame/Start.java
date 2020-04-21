8d50fca424bc49aaafc1d08f810dd8ba268db0d7
ahncheer <ahncheer@naver.com>
2020-04-21 13:21:14 +0900

    2020-04-21 || 13:20 백업

---
+++ blockgame2/src/main/java/com/gmail/amu30141592/go20200421/blockgame/Start.java
@@ -0,0 +1,14 @@
+package com.gmail.amu30141592.go20200421.blockgame;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+    }
+}
