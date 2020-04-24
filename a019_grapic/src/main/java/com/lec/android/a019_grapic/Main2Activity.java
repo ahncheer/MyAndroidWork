d03f73a8ea94a5712332036870e550b14ffefb8f
ahncheer <ahncheer@naver.com>
2020-04-24 17:49:02 +0900

    2020-04-24 || 18:00 백업

--- a019_grapic/src/main/java/com/lec/android/a019_grapic/Main2Activity.java
+++ a019_grapic/src/main/java/com/lec/android/a019_grapic/Main2Activity.java
@@ -1,39 +1,73 @@
 package com.lec.android.a019_grapic;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-    }
-    class MyView2 extends View{
+        //setContentView(R.layout.activity_main2);
 
-        public MyView2(Context context) {
-            super(context);
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            Paint paint = new Paint();
-
-            paint.setColor(Color.RED);
-            canvas.drawRect(x-100, y-100, x+100, y+100, paint);
-
-            paint.setColor(Color.BLUE);
-            paint.setTextSize(40);
-            canvas.drawText("글씨", 50, 50, paint);
-        }
+        setContentView(new MyView2(this));
     }
 }
+
+class MyView2 extends View {
+    int x = 100, y = 100;
+
+    public MyView2(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+
+
+        paint.setColor(Color.BLUE);
+        paint.setTextSize(80);
+        canvas.drawText("글씨", 300, 300, paint);
+
+        paint.setColor(Color.RED);
+        canvas.drawRect(x - 100, y - 100, x + 100, y + 100, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+                x = (int)event.getX();
+                y = (int)event.getY();
+                invalidate();  // 화면을 다시 그려주기 --> onDraw() 호출함.
+        }
+
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
